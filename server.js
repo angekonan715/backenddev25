8bb7306aa99efb7799e836927a61498a3e9528dd
angekonan715 <kou20004@byui.edu>
2025-05-19 03:24:36 -0600

    getting started

--- server.js
+++ server.js
@@ -6,14 +6,28 @@
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts") // we tell the application to require express-ejs-layouts, so it can be used.
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+
+
+/* ***********************
+ * View Engine and Templates
+ *************************/
+
+app.set("view engine", "ejs") // declare the view engine to be ejs
+app.use(expressLayouts) // use express-ejs-layouts
+app.set("layout", "layouts/layout") // not at views root :
 
 /* ***********************
  * Routes
  *************************/
 app.use(static)
+// Index Route
+app.get("/", function (req, res) {
+  res.render("index", { title: "Home" })
+})
 
 /* ***********************
  * Local Server Information
